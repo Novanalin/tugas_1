7fc8826f5ba2e4989169e298b8718a9dcd1263b1
Novana <novana.lin@gmail.com>
2024-10-08 20:08:49 +0700

    Update data main

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,175 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(KuisApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class KuisApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kuis Sederhana',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MulaiKuisPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+// Halaman untuk memulai kuis
+class MulaiKuisPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Kuis Sederhana'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Selamat Datang di Kuis Sederhana!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => KuisPage()),
+                );
+              },
+              child: Text('Mulai Kuis'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// Halaman untuk pertanyaan kuis
+class KuisPage extends StatefulWidget {
+  @override
+  _KuisPageState createState() => _KuisPageState();
+}
+
+class _KuisPageState extends State<KuisPage> {
+  final List<Question> questions = [
+    Question(
+      question: "Apa yang menjadi simbol kimia untuk air?",
+      options: ["O2", "CO2", "H2O"],
+      answer: "H2O",
+    ),
+    Question(
+      question: "Apa warna bendera nasional Jepang?",
+      options: ["Merah dan Putih", "Biru dan Putih", "Hijau dan Putih"],
+      answer: "Merah dan Putih",
+    ),
+    Question(
+      question: "Apa nama planet ketiga dari Matahari dalam tata surya kita?",
+      options: ["Mars", "Venus", "Bumi"],
+      answer: "Bumi",
+    ),
+    Question(
+      question: "Siapa penemu lampu pijar?",
+      options: ["Nikola Tesla", "Thomas Edison", "Albert Einstein"],
+      answer: "Thomas Edison",
+    ),
+    Question(
+      question: "Apa yang dimaksud dengan Global Warming?",
+      options: [
+        "Proses pendinginan atmosfer Bumi",
+        "Kenaikan suhu rata-rata Bumi karena gas rumah kaca",
+        "Perubahan musim yang ekstrem"
+      ],
+      answer: "Kenaikan suhu rata-rata Bumi karena gas rumah kaca",
+    ),
+  ];
+
+  int currentQuestionIndex = 0;
+  int score = 0;
+
+  void answerQuestion(String selectedOption) {
+    if (selectedOption == questions[currentQuestionIndex].answer) {
+      score++;
+    }
+    if (currentQuestionIndex < questions.length - 1) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    } else {
+      _showScore();
+    }
+  }
+
+  void _showScore() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Skor Anda'),
+          content: Text('Anda mendapatkan $score dari ${questions.length} pertanyaan.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  currentQuestionIndex = 0;
+                  score = 0;
+                });
+              },
+              child: Text('Ulangi Kuis'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pop(); // Kembali ke halaman Mulai Kuis
+              },
+              child: Text('Keluar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kuis Sederhana'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              questions[currentQuestionIndex].question,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ...questions[currentQuestionIndex].options.map((option) {
+              return ElevatedButton(
+                onPressed: () => answerQuestion(option),
+                child: Text(option),
+              );
+            }).toList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Question {
+  final String question;
+  final List<String> options;
+  final String answer;
+
+  Question({required this.question, required this.options, required this.answer});
 }
